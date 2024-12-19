ed62dc154d83d3d212ab2aebea61d01d548892c4
BugHunter-X <askzenai@gmail.com>
2024-12-19 04:58:03 +0100

    Add bug.ts file

---
+++ bug.ts
@@ -0,0 +1,10 @@
+function printString(str: string): void {
+  console.log(str);
+}
+
+function usePrintString(): void {
+  const myNumber: number = 10;
+  printString(myNumber); // Error: Argument of type 'number' is not assignable to parameter of type 'string'.
+}
+
+usePrintString();
